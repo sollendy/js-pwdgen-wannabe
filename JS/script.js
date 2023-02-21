daeaf207124c597724ca2a5f7c5f1dd0a45bfdd6
sollendy <edlannino@tutanota.com>
2023-02-21 15:09:23 +0100

    esercizio terminato (?)

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("come ti chiami?");
+//document.writeln (nome);
+let cognome = prompt('come fai di cognome?')
+//document.writeln (cognome);
+let colore = prompt('qual è il tuo colore preferito?')
+//document.writeln (colore);
+document.writeln (nome + cognome + colore + '69')
